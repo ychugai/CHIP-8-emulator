7fd33785f4831d9f6e760a2929142c8729226b28
kedsup <v.kanaeva2012@gmail.com>
2019-06-05 00:19:48 +0300

    Improved comments

--- src/js/chip8.js
+++ src/js/chip8.js
@@ -180,76 +180,61 @@ class Chip8 {
     switch (opcode & 0xf000) {
       case 0x0000:
         switch (opcode) {
-          // CLS
-          // OOE0
-          // CLear the display.
+          
+            // 00E0 - Clear the screen and move to next instruction
           case 0x00E0:
             this.renderer.clear();
             for (let i = 0; i < this.display.length; i++) {
               this.display[i] = 0;
             }
             break;
-            // RET
-            // 00EE
-            // Return from subroutine.
+            
+            // 00EE- Return from subroutine and move to next instruction
           case 0x00EE:
             this.pc = this.stack[--this.sp];
             break;
         }
         break;
-        // JP addr
-        // 1nnnn
-        // Jump to location nnn
+        
+        // 1nnn - Jump to instruction located at nnn
       case 0x1000:
         this.pc = opcode & 0xFFF;
         break;
 
-        // CALL addr
-        // 2nnnn
-        // Call subroutine at nnnn.
+        // 2nnn - Call subroutine located at nnn, saving previous location on stack
       case 0x2000:
         this.stack[this.sp] = this.pc;
         this.sp++;
         this.pc = opcode & 0x0FFF;
         break;
 
-        // SE Vx, byte
-        // 2xkk
-        // Skip next instruction if vX equals kk.
+        // 3xkk - Skip next instruction if Vx === kk
       case 0x3000:
         if (this.v[x] === (opcode & 0xFF)) {
           this.pc += 2;
         }
         break;
 
-        // SNE Vx, byte
-        // 4xkk
-        // Skip next instruction if vX doesn't equal kk.
+        // 4xkk - Skip next instruction if Vx !== kk
       case 0x4000:
         if (this.v[x] != (opcode & 0x00FF)) {
           this.pc += 2;
         }
         break;
 
-        // SE Vx, Vy
-        // 5xy0
-        // Skip next instruction if vX equals vY.
+        // 5xy0 - Skip next instruction if Vx === Vy
       case 0x5000:
         if (this.v[x] === this.v[y]) {
           this.pc += 2;
         }
         break;
 
-        // LD Vx, byte
-        // 6xkk
-        // Set Vx equal to kk.
+        // 6xkk - Set Vx = kk
       case 0x6000:
         this.v[x] = opcode & 0xFF;
         break;
 
-        // ADD Vx, byte
-        // 7xkk
-        // Set Vx equal to Vx + kk.
+        // 7xkk - Set Vx = Vx + kk
       case 0x7000:
         let val = (opcode & 0xFF) + this.v[x];
 
@@ -264,37 +249,27 @@ class Chip8 {
 
         switch (opcode & 0x000f) {
 
-          // LD Vx, Vy
-          // 8xy0
-          // Stores register Vy in Vx
+            // 8xy0 - Set Vx = Vy
           case 0x0000:
             this.v[x] = this.v[y];
             break;
 
-            // OR Vx, Vy
-            // 8xu1
-            // Set vX equal to vX OR Vy;
+            // 8xy1 - Set Vx = Vy OR Vx
           case 0x0001:
             this.v[x] |= this.v[y];
             break;
 
-            // AND Vx, Vy
-            // 8xy2
-            // Set Vx equal to Vx AMD Vy
+            // 8xy2 - Set Vx = Vy AND Vx
           case 0x0002:
             this.v[x] &= this.v[y];
             break;
 
-            // XOR Vx, Vy
-            // 8xy3
-            // Set Vx equal to Vx XOR Vy.
+            // 8xy3 - Set Vx = Vy XOR Vx
           case 0x0003:
             this.v[x] ^= this.v[y];
             break;
 
-            // ADD Vx, Vy
-            // 8xy4
-            // Set Vx equal to Vx + Vy, set Vf equal to carry.
+            // 8xy4 - Set Vx = Vx + Vy, set Vf = 1 if Vx + Vy > 255
           case 0x0004:
             this.v[x] += this.v[y];
             this.v[0xF] = +(this.v[x] > 255);
@@ -303,9 +278,7 @@ class Chip8 {
             }
             break;
 
-            // SUB Vx, Vy
-            // 8xy5
-            // Set Vx equal to Vx - Vy, set Vf equal to NOT borrow.
+            // 8xy5 - If Vx > Vy, set Vf = 0, otherwise 1. Set Vx = Vx - Vy
           case 0x0005:
             this.v[0xF] = +(this.v[x] > this.v[y]);
             this.v[x] -= this.v[y];
@@ -314,17 +287,13 @@ class Chip8 {
             }
             break;
 
-            // SHR Vx, Vy
-            // 8xy6
-            // Set Vx SHR 1.
+            // 8xy6 - If the least-significant bit of Vx is 1, set VF = 1, else 0. Vx /= 2
           case 0x0006:
             this.v[0xF] = this.v[x] & 0x1;
             this.v[x] >>= 1;
             break;
 
-            // SUBN Vx, Vy
-            // 8xy7
-            // Set Vx equal to Vy - Vx, set Vf equal to NOT borrow.
+            // 8xy7 - If Vy > Vx, set Vf = 1, else 0. Vx = Vy - Vx
           case 0x0007:
             this.v[0xF] = +(this.v[y] > this.v[x]);
             this.v[x] = this.v[y] - this.v[x];
@@ -333,10 +302,7 @@ class Chip8 {
             }
             break;
 
-
-            // SHL Vx, Vy
-            // 8xyE
-            // Set Vx equal to Vx SHL 1.
+            // 8xyE - If the most-significant bit of Vx is 1, set VF = 1, else 0. Vx *= 2
           case 0x000E:
             this.v[0xF] = +(this.v[x] & 0x80);
             this.v[x] <<= 1;
@@ -346,39 +312,31 @@ class Chip8 {
             break;
         }
         break;
-        // SNE Vx, Vy
-        // 9xy0
-        // Skip next instruction if Vx is not equal to Vy.
+
+        // 9xy0 - Skip next instruction if Vx !== Vy
       case 0x9000:
         if (this.v[x] != this.v[y]) {
           this.pc += 2;
         }
         break;
 
-        // LD I, addr
-        // Annn
-        // Set I equal to nnn.
+        // Annn - Set I = nnn
       case 0xA000:
         this.i = opcode & 0xFFF;
         break;
 
-        // JP V0, addr
-        // Bnnn
-        // Jump to location V0 + nnn.
+        // Bnnn - Jump to instruction located at nnn + V0
       case 0xB000:
         this.pc = (opcode & 0xFFF) + this.v[0];
         break;
 
-        // RND Vx, byte
-        // Cxkk
-        // Set Vx equal to random byte AND kk.
+        // Cxkk - Set Vx = to kk AND random byte
       case 0xC000:
         this.v[x] = Math.floor(Math.random() * 0xFF) & (opcode & 0xFF);
         break;
 
-        // DRW Vx, Vy, nibble
-        // Dxyn
-        // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF equal to collision.
+        // Dxyn - Display at coordinate (Vx, Vy) n-byte sprite starting at 
+	      // location stored in I. Set Vf = 1 if collision occurs, else 0.
       case 0xD000:
         this.v[0xF] = 0;
 
@@ -404,18 +362,14 @@ class Chip8 {
       case 0xE000:
         switch (opcode & 0x00FF) {
 
-          // SKP Vx
-          // Ex9E
-          // Skip next instruction if the key with the value Vx is pressed.
+          // Ex9E - Skip next instruction if key with value stored in Vx is pressed
           case 0x009E:
             if (this.keys[this.v[x]]) {
               this.pc += 2;
             }
             break;
 
-            // SKNP Vx
-            // ExA1
-            // Skip  next instruction if the key with the value Vx is NOT pressed.
+            // ExA1 - Skip next instruction if key with value stored in Vx is NOT pressed
           case 0x00A1:
             if (!this.keys[this.v[x]]) {
               this.pc += 2;
@@ -430,16 +384,12 @@ class Chip8 {
 
         switch (opcode & 0x00FF) {
 
-          // LD Vx, DT
-          // Fx07
-          // Place value of DT in Vx.
+          // Fx07 - Set Vx = value stored in delay timer
           case 0x0007:
             this.v[x] = this.delayTimer;
             break;
 
-            // LD Vx, K
-            // Fx0A
-            // Wait for keypress, then store it in Vx.
+            // Fx0A - Wait for a key press, store value of the key in Vx
           case 0x000A:
 
             const oldKeyDown = this.setKey;
@@ -457,9 +407,7 @@ class Chip8 {
             this.stop();
             return;
 
-            // LD DT, Vx
-            // Fx15
-            // DT is set to Vx.
+            // Fx15 - Set delay timer = Vx
           case 0x0015:
             this.delayTimer = this.v[x];
             break;
@@ -471,24 +419,18 @@ class Chip8 {
           //   this.soundTimer = this.v[x];
           //   break;
 
-            // ADD I, Vx
-            // Fx1E
-            // Set I equal to I + Vx
+            // Fx1E - Set I = I + Vx
           case 0x001E:
             this.i += this.v[x];
             break;
 
-            // LD F, Vx
-            // Fx29
-            // Set I equal to location of sprite for digit Vx.
+            // Fx29 - Set I = location of sprite for digit Vx
           case 0x0029:
             // Multiply by number of rows per character.
             this.i = this.v[x] * 5;
             break;
 
-            // LD B, Vx
-            // Fx33
-            // Store BCD representation of Vx in memory location starting at location I.
+            // Fx33 - Store BCD represntation of Vx in locations I, I+1, I+2
           case 0x0033:
             let number = this.v[x], i;
 
@@ -498,18 +440,14 @@ class Chip8 {
             }
             break;
 
-            // LD [I], Vx
-            // Fx55
-            // Store registers V0 through Vx in memory starting at location I.
+            // Fx55 - Store registers V0 through Vx in memory starting at location I
           case 0x0055:
             for (let i = 0; i <= x; i++) {
               this.memory[this.i + i] = this.v[i];
             }
             break;
 
-            // LD Vx, [I]
-            // Fx65
-            // Read registers V0 through Vx from memory starting at location I.
+            // Fx65 - Read registers V0 through Vx from memory starting at location I
           case 0x0065:
             for (let i = 0; i <= x; i++) {
               this.v[i] = this.memory[this.i + i];
